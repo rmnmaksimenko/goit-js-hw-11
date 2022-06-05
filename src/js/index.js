8a7c5070f7e420d60834779c2c869596e35f75b5
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-06 00:10:15 +0300

    1.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
+import Notiflix from 'notiflix';
 
 const PIXA_URL = 'https://pixabay.com/';
 const searchArea = document.querySelector('.search-form');
@@ -6,6 +7,7 @@ const galleryHTML = document.querySelector('.gallery');
 const searchMoreButton = document.querySelector('.search-more');
 
 let pageNumber = null;
+let currentSearchValue = '';
 
 searchArea.addEventListener('click', startSearch);
 
@@ -21,6 +23,7 @@ async function startSearch(e) {
     return;
   }
   // console.log(options);
+  currentSearchValue = searchArea[0].value;
   console.log(searchMoreButton.classList);
   pageNumber = 1;
   search();
@@ -38,26 +41,43 @@ async function searchMore() {
 
 async function search() {
   const responce = await fetch(
-    `${PIXA_URL}api/?${options}&q=${searchArea[0].value}&page=${pageNumber}&per_page=40`
+    `${PIXA_URL}api/?${options}&q=${currentSearchValue}&page=${pageNumber}&per_page=40`
   );
-  // console.log(responce);
+  console.log(responce);
   const pictures = await responce.json();
-  console.log(pictures.hits);
+  console.log(pictures);
   if (pictures.hits.length === 0) {
     ClearSearch();
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
     return;
+  }
+  if (pageNumber === 1) {
+    Notiflix.Notify.success(`We found ${pictures.totalHits} pictrues!`);
   }
   let galleryBLock = '';
   for (let i = 0; i < pictures.hits.length; i++) {
     const picture = `<div class='gallery__item'><a class="gallery__link" href="${pictures.hits[i].largeImageURL}">
                 <img class="gallery__image" src="${pictures.hits[i].webformatURL}" data-source="${pictures.hits[i].largeImageURL}" alt="${pictures.hits[i].tags}">
-            </a></div>`;
+            <div class='gallery__box'>
+            <ul>
+            <li><h2 class='gallery-header'>Likes</h2><p class='gallery-text'>${pictures.hits[i].likes}</p></li>
+            <li><h2 class='gallery-header'>Views</h2><p class='gallery-text'>${pictures.hits[i].views}</p></li>
+            <li><h2 class='gallery-header'>Comments</h2><p class='gallery-text'>${pictures.hits[i].comments}</p></li>
+            <li><h2 class='gallery-header'>Downloads</h2><p class='gallery-text'>${pictures.hits[i].downloads}</p></li>
+            </ul>
+            </div></a>
+            </div>`;
     galleryBLock += picture;
   }
-  if (pictures.hits.length === 40) {
+  if ((pictures.hits.length === 40) & (pageNumber * 40 < pictures.totalHits)) {
     searchMoreButton.disabled = false;
     searchMoreButton.classList.remove('is-hidden');
   } else {
+    Notiflix.Notify.warning(
+      "We're sorry, but you've reached the end of search results."
+    );
     searchMoreButton.classList.add('is-hidden');
   }
   galleryHTML.insertAdjacentHTML('beforeend', galleryBLock);
